08c19a91a1bfe1186cace5117acf11d87344132f
sergumen1-ui <sergumen1@gmail.com>
2026-05-14 10:34:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "tracker-v1";
-const FILES = ["/", "/index.html", "/manifest.json"];
+const FILES = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -15,6 +15,6 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("/index.html")))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./index.html")))
   );
 });
